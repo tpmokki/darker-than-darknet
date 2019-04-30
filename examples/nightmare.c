3e64e747085cb6e8c4edd561fcd7dc2b1b833c11
Teemu Pesonen <musta.pippuri@gmail.com>
2019-04-30 09:39:01 +0000

    Digging deeper bones

--- examples/nightmare.c
+++ examples/nightmare.c
@@ -173,7 +173,6 @@ void reconstruct_picture(network *net, float *features, image recon, image updat
     }
 }
 
-/*
 void run_lsd(int argc, char **argv)
 {
     srand(0);
@@ -279,7 +278,6 @@ void run_lsd(int argc, char **argv)
         im = resized;
     }
 }
-*/
 
 void run_nightmare(int argc, char **argv)
 {
