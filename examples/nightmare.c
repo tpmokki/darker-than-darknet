4cf4fa0ef53975f87c81115e861ffc7d8c1a5b4c
Dark Guy <dark.guy@softargam.com>
2019-04-30 09:39:09 +0000

    Do

--- examples/nightmare.c
+++ examples/nightmare.c
@@ -173,6 +173,7 @@ void reconstruct_picture(network *net, float *features, image recon, image updat
     }
 }
 
+/*
 void run_lsd(int argc, char **argv)
 {
     srand(0);
@@ -278,6 +279,7 @@ void run_lsd(int argc, char **argv)
         im = resized;
     }
 }
+*/
 
 void run_nightmare(int argc, char **argv)
 {
